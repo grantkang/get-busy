55b86f2fb35bf9c4566d181dd9cb4a628f47037d
Grant Kang <3s.grantkang@gmail.com>
2020-03-26 13:28:04 -0700

    Added yelp search component class file

---
+++ components/yelp-search.js
@@ -0,0 +1,23 @@
+
+class YelpSearch {
+  constructor(yelpSearchFormElement) {
+    this.formElement = yelpSearchFormElement;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.formElement.addEventListener('submit', this.handleSubmit);
+  }
+  onSubmit(searchForPotentialCustomers) {
+    this.searchForPotentialCustomers = searchForPotentialCustomers;
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    var formData = new FormData(event.target);
+    var yelpBusinessSearchRequest = {
+      location: formData.get('location'),
+      optionalParams: {
+        'term': formData.get('search-query'),
+        'categories': formData.get('search-query')
+      }
+    }
+    this.searchForPotentialCustomers(yelpBusinessSearchRequest);
+  }
+}
