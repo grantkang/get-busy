0f6435fe0f3033784fb7d7b1624ccf722a86a393
Grant Kang <3s.grantkang@gmail.com>
2020-05-23 19:28:44 -0700

    App component now opens up the notification modal with the intro info

--- client/components/app.js
+++ client/components/app.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-undef */
 class App {
-  constructor(routingForm, mapController, yelpSearchForm) {
+  constructor(routingForm, mapController, yelpSearchForm, notificationService) {
     this.routingForm = routingForm;
     this.mapController = mapController;
     this.yelpSearchForm = yelpSearchForm;
+    this.notificationService = notificationService;
   }
 
   calculateRoute = directionsRequest => {
@@ -32,6 +33,19 @@ class App {
     this.mapController.initMap();
     this.routingForm.onSubmit(this.calculateRoute);
     this.yelpSearchForm.onSubmit(this.searchForPotentialCustomers);
+    this.notificationService.open(
+      'Welcome',
+      [
+        'Get Busy is an app that helps you find the shortest route to make a roundtrip while visiting all the places you need to go!',
+        'If this is your first time here. Here\'s what you need to know to get started:',
+        '1.) All that app needs to know is your starting/ending point & some stops.',
+        '2.) For either input, you can either put the name of the place or the address.',
+        '3.) Optionally, you can use the input form on the nav bar to search for places to set as your stops.'
+      ],
+      [
+        { label: 'Got It!' }
+      ]
+    );
   }
 
   handlePotentialCustomerSearchSuccess = resp => {
--- client/index.js
+++ client/index.js
@@ -2,18 +2,22 @@ const RoutingForm = require('./components/routing-form');
 const MapController = require('./components/map-controller');
 const YelpSearch = require('./components/yelp-search');
 const App = require('./components/app');
+const NotificationService = require('./components/notification-service');
 
-var routeForm;
-var mapController;
-var yelpSearchForm;
-var app;
+let notificationService;
+let routeForm;
+let mapController;
+let yelpSearchForm;
+let app;
 
 // eslint-disable-next-line no-unused-vars
 function init() {
+  notificationService = new NotificationService(document.getElementById('modal-overlay'));
   routeForm = new RoutingForm(document.getElementById('routing-form'));
   mapController = new MapController(document.getElementById('map'));
   yelpSearchForm = new YelpSearch(document.getElementById('yelp-search-form'));
-  app = new App(routeForm, mapController, yelpSearchForm);
+
+  app = new App(routeForm, mapController, yelpSearchForm, notificationService);
   app.start();
 }
 
